a5a2e82714d49f0de666192300a16c7108fb8ba4
Marc Phillips <marphill@adobe.com>
2014-03-11 12:54:49 -0600

    Added test files to repository.

---
+++ test/derigible/transactions/TransactionTest.java
@@ -0,0 +1,14 @@
+package derigible.transactions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransactionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
